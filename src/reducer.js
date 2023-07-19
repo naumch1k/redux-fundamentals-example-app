2f4ac8314bd08ea86498231334e0e19bdaf3aba3
Irina Naumchik <irina.naumchik2012@yandex.ru>
2023-07-19 18:16:37 -0500

    feat: add reducer case for todoToggled action

--- src/reducer.js
+++ src/reducer.js
@@ -30,6 +30,21 @@ const appReducer = (state = initialState, action) => {
         ]
       }
     }
+    case 'todos/todoToggled': {
+      return {
+        ...state,
+        todos: state.todos.map(todo => {
+          if (todo.id !== action.payload) {
+            return todo
+          }
+
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
+        })
+      }
+    }
     default:
       return state
   }
