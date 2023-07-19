d11c25154bf4b3bab24b7ef55e541666a70114c1
Irina Naumchik <irina.naumchik2012@yandex.ru>
2023-07-19 17:51:45 -0500

    feat: add reducer case for todoAdded action

--- src/reducer.js
+++ src/reducer.js
@@ -10,8 +10,26 @@ const initialState = {
   }
 }
 
+const getNextTodoId = (todos) => {
+  const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
+  return maxId + 1
+}
+
 const appReducer = (state = initialState, action) => {
   switch (action.type) {
+    case 'todos/todoAdded': {
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          {
+            id: getNextTodoId(state.todos),
+            text: action.payload,
+            completed: false,
+          }
+        ]
+      }
+    }
     default:
       return state
   }
