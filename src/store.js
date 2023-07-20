c21d8cb8ae9253dfe8183224429f647fd8589891
Irina Naumchik <irina.naumchik2012@yandex.ru>
2023-07-20 10:37:45 -0500

    feat: use composeWithDevTools to enable the DevTools Extension

--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
 import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducer'
 import { delayedMessageMiddleware } from './exampleAddons/middleware'
 
-const middlewareEnhancer = applyMiddleware(delayedMessageMiddleware)
+const middlewareEnhancer = composeWithDevTools(
+  applyMiddleware(delayedMessageMiddleware)
+)
 
 const store = createStore(rootReducer, middlewareEnhancer)
 
