09da95d0423f214f2201cbe56dc04cb48ab8ca2c
Irina Naumchik <irina.naumchik2012@yandex.ru>
2023-07-20 19:05:33 -0500

    feat: use react-redux hooks and Provider in UI

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import TodoListItem from './TodoListItem'
 
 const TodoList = () => {
-  const todos = []
+  const todos = useSelector(state => state.todos)
 
   return (
     <ul className='todo-list'>
--- src/index.js
+++ src/index.js
@@ -1,28 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './App'
-
 import './api/server'
-
 import store from './store'
-
-console.log('initial state: ', store.getState())
-
-const unsubscribe = store.subscribe(() =>
-  console.log('state after dispatch: ', store.getState())
-)
-
-store.dispatch({
-  type: 'todos/todoAdded',
-  payload: 'Learn about actions'
-})
-
-unsubscribe()
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
