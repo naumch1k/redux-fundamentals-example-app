e8648bbc4f980494ac2216f23b557976a22efab2
Irina Naumchik <irina.naumchik2012@yandex.ru>
2023-07-24 12:46:26 -0500

    feat: show loading indicator in the UI

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -5,6 +5,15 @@ import TodoListItem from './TodoListItem'
 
 const TodoList = () => {
   const todoIds = useSelector(selectFilteredTodoIds)
+  const loadingStatus = useSelector(state => state.todos.status)
+
+  if (loadingStatus === 'loading') {
+    return (
+      <div className="todo-list">
+        <div className="loader" />
+      </div>
+    )
+  }
 
   return (
     <ul className='todo-list'>
